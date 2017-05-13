ad5a968e947534ed5d7613958333c9a23c201221
khubruy <khubruy@gmail.com>
2017-05-13 12:37:34 +0530

    user form working with image uploads as well

--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -29,8 +29,8 @@
             {!! Form::password('password', ['class' => 'form-control']) !!}
         </div>
         <div class="form-group">
-            {!! Form::label('file', 'Upload') !!}
-            {!! Form::file('file',['class'=>'form-control']) !!}
+            {!! Form::label('photo_id', 'Upload') !!}
+            {!! Form::file('photo_id',['class'=>'form-control']) !!}
         </div>
 
 
