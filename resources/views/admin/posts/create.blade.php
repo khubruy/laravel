f5e925765b2095c96d019134b5a50106c1ad0325
khubruy <khubruy@gmail.com>
2017-05-20 11:44:58 +0530

    able to create posts with static category

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -1,7 +1,37 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Dhwani
- * Date: 09-05-2017
- * Time: 14:36
- */
+@extends('layouts.admin')
+
+
+
+@section('content')
+
+    <h1>Create Posts</h1>
+
+    {!! Form::open(['method'=>'POST','action'=>'AdminPostsController@store','files'=>'true']) !!}
+    <div class="form-group">
+        {!! Form::label('title', 'Title') !!}
+        {!! Form::text('title', null, ['class'=>'form-control'] ) !!}
+    </div>
+
+    <div class="form-group">
+        {!! Form::label('category_id', 'Category') !!}
+        {!! Form::select('category_id', ['0'=>'PHP','1'=>'Javascript'] ,null, ['class'=>'form-control'] ) !!}
+    </div>
+
+    <div class="form-group">
+        {!! Form::label('photo_id', 'Photo') !!}
+        {!! Form::file('photo_id' ,null, ['class'=>'form-control'] ) !!}
+    </div>
+
+    <div class="form-group">
+        {!! Form::label('body', 'Body') !!}
+        {!! Form::textarea('body', null, ['class'=>'form-control','rows'=>4] ) !!}
+    </div>
+
+
+    <div class="form-group">
+        {!! Form::submit('Create Post', ['class'=>'btn btn-primary'] ) !!}
+    </div>
+
+    @include('includes.validation')
+    {!! Form::close() !!}
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -24,6 +24,7 @@ Route::get('/home', 'HomeController@index')->name('home');
 Route::group(['middleware'=>'admin'], function(){
     Route::resource('admin/users','AdminUsersController');
 
+    Route::resource('admin/posts','AdminPostsController');
 
 
     Route::get('/admin', function () {
