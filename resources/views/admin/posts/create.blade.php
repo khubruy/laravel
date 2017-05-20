772ea35014621d855b1c5db6f003d54aed9aa31f
khubruy <khubruy@gmail.com>
2017-05-20 12:16:58 +0530

    able to create posts with dynamic categories

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -14,7 +14,7 @@
 
     <div class="form-group">
         {!! Form::label('category_id', 'Category') !!}
-        {!! Form::select('category_id', ['0'=>'PHP','1'=>'Javascript'] ,null, ['class'=>'form-control'] ) !!}
+        {!! Form::select('category_id', [''=>'Choose Category'] + $categories ,null, ['class'=>'form-control'] ) !!}
     </div>
 
     <div class="form-group">
