6eb3067094d9eee20c7e2f5273c096046100c98e
cookie <khubruy@gmail.com>
2017-06-16 07:45:08 -0700

    disqus part done

--- routes/web.php
+++ routes/web.php
@@ -44,3 +44,7 @@ Route::group(['middleware'=>'admin'], function(){
 Route::group(['middleware'=>'auth'], function() {
     Route::post('comment/reply','CommentRepliesController@createReply');
 });
+
+Route::get('/disqus', function () {
+    return view('disqus');
+});
